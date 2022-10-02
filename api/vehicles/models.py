cf967f3d857b7983e3bec5e531f4248a39c1c6c2
crismigo <cristinamigo01@gmail.com>
2022-10-02 19:59:49 +0200

    Tasca #23: modificació al model Vehicles, he afegit una unique clause

--- api/vehicles/models.py
+++ api/vehicles/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 from api.users.models import Users
-#from api.chargers.models import ChargersType
+# from api.chargers.models import ChargersType
 from config import settings
 
 
 # Create your models here.
 class CarsBrand(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False, unique=True)
 
     class Meta:
         verbose_name = "CarBrand"
@@ -22,11 +22,13 @@ class CarsModel(models.Model):
     year = models.DateField(null=True, blank=True)
     autonomy = models.FloatField(null=True, blank=True)
     car_brand = models.ForeignKey(CarsBrand, on_delete=models.CASCADE, null=False, blank=False)
-    #charger_type = models.ManyToManyField(ChargersType)
+
+    # charger_type = models.ManyToManyField(ChargersType)
 
     class Meta:
         verbose_name = "CarModel"
         verbose_name_plural = "CarsModel"
+        unique_together = ["car_brand", "name", "year"]
 
     def __str__(self):
         return self.name
@@ -35,7 +37,7 @@ class CarsModel(models.Model):
 class Cars(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     charge_capacity = models.FloatField(null=False, blank=False, default=-1)
-    car_license = models.CharField(max_length=7, null=False, blank=False)
+    car_license = models.CharField(max_length=10, null=False, blank=False)
     model_id = models.ForeignKey(CarsModel, on_delete=models.CASCADE, null=False, blank=False)
     car_owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, blank=False)
 
