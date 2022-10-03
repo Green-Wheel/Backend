5c1355077a4c384e5bf96a80be911120f30083f0
crismigo <cristinamigo01@gmail.com>
2022-10-03 16:08:42 +0200

    Tasca #27: creat el model de Ratings

--- api/ratings/models.py
+++ api/ratings/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from config import settings
+
+
 # Create your models here.
+class Ratings(models.Model):
+    rate = models.IntegerField(null=False, blank=False)
+    comment = models.TextField(null=True, blank=True)
+    # booking = Associar-ho a una reservaFinalitzada (veure com ho fa qui faci el model de reserva)
+
+    class Meta:
+        verbose_name = "Rating"
+        verbose_name_plural = "Ratings"
+        abstract = True
+
+    def __str__(self):
+        return self.id
+
+
+class ClientsRating(Ratings):
+    client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, blank=False)
+
+
+# class PostRating(Ratings):
+#  post = models.ForeignKey(Post, on_delete=models.CASCADE, null=False, blank=False)
