82ee39d55fb443c1cf7e86994105708040402e43
Andreu Orensanz <andreu.orensanz@estudiantat.upc.edu>
2022-10-13 20:44:47 +0200

    Tasca #150: migrations reserves i views (falta revisar i que funcioni el migrate)

--- api/bookings/models.py
+++ api/bookings/models.py
@@ -1,3 +1,23 @@
+from config import settings
 from django.db import models
+from api.users.models import Users
+from api.chargers.models import Publication
+
 
 # Create your models here.
+class Bookings(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False, blank=False)
+    publication = models.ForeignKey(Publication, on_delete=models.CASCADE, null=False, blank=False)
+    start_date = models.DateTimeField(null=False, blank=False)
+    end_date = models.DateTimeField(null=False, blank=False)
+    confirmed = models.BooleanField(default=False)
+    finished = models.BooleanField(default=False)
+    cancelled = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Booking"
+        verbose_name_plural = "Bookings"
+
+    def __str__(self):
+        return self.id
--- api/bookings/views.py
+++ api/bookings/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status
+
+from .models import Bookings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 # Create your views here.
+class BookingsApiView(APIView):
+    def get(self, request, user_id):
+        booking_instance = Bookings.objects.filter(user=user_id)
+        if not booking_instance:
+            return Response(
+                {"res": "Booking with the id doesn't exist"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return Response(request, {'bookings': booking_instance})
+        # return Response({"booking": booking_instance}, status=status.HTTP_200_OK)
