83cd919cc1baad5d12dbc134e88945df533954b6
Àlex Ollé <alex.olle@estudiantat.upc.edu>
2022-10-22 09:56:14 +0200

    Afegit latitud i longitud que necessitava l'andreu

--- api/chargers/serializers.py
+++ api/chargers/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from api.chargers.models import PublicChargers
+from api.chargers.models import PublicChargers, Localizations
+
+
+class LocalizationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =Localizations
+        fields = ["latitude", "longitude"]
 
 
 class PublicChargerSerializer(serializers.ModelSerializer):
+    localization = serializers.SerializerMethodField("get_localization")
+
+    def get_localization(self, obj):
+        return LocalizationsSerializer(obj.localization).data
+
     class Meta:
         model = PublicChargers
-        fields = ["title", "description", "direction", "town", "localization", "power", "speed", "connection_type", "current_type", "agent", "identifier", "access", "available"]
+        fields = ["title", "description", "direction", "town", "localization", "power", "speed", "connection_type",
+                  "current_type", "agent", "identifier", "access", "available"]
