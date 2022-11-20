48e930000f75e90965a5be15fc19c1821b421ad6
aolle99 <aolle99@gmail.com>
2022-11-20 14:36:07 +0100

    Tasca #178: S'han començat a desenvolupar els serveis i la view de bikes

---
+++ api/bikes/services.py
@@ -0,0 +1,38 @@
+from api.bikes.models import BikeTypes, Bikes
+
+
+def __get_filter(filter_params):
+    filters = {}
+    if filter_params.get('town'):
+        filters['town__id'] = filter_params['town']
+    if filter_params.get('bike_type'):
+        filters['bike_type__id'] = filter_params['bike_type']
+    if filter_params.get('price'):
+        filters['price__lte'] = filter_params['price']
+    if filter_params.get('power'):
+        filters['power__gte'] = filter_params['power']
+    return filters
+
+
+def get_all_bikes():
+    return Bikes.objects.all()
+
+
+def get_bike_by_id(id):
+    return Bikes.objects.get(id=id)
+
+
+def get_filtered_bikes(filter_params):
+    filters = __get_filter(filter_params)
+    bikes = Bikes.objects.filter(**filters)
+    order = filter_params.get('order', None)
+    if order:
+        bikes = bikes.order_by(order)
+    return bikes
+
+
+def get_bikes_type():
+    return BikeTypes.objects.all()
+
+def create_bike(data, user):
+    pass
--- api/bikes/views.py
+++ api/bikes/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.bikes.serializers import BikeSerializer, DetailedBikeSerializer
+from api.bikes.services import get_filtered_bikes, create_bike
+
 
 # Create your views here.
+class BikesView(APIView):
+    def get(self, request):
+        try:
+            bikes = get_filtered_bikes(request.query_params)
+            serializer = BikeSerializer(bikes, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        try:
+            new_bike = create_bike(request.data, request.user.id)
+            # add charger to user
+            return Response(DetailedBikeSerializer(new_bike).data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"res": "Error: " + str(e)}, status=status.HTTP_400_BAD_REQUEST)
