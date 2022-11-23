d3fd7426ddaa0727a2eebe80786cd9a7d364bdbd
crismigo <cristinamigo01@gmail.com>
2022-11-23 16:14:57 +0100

    task #182: testing

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,32 +8,32 @@ set -o pipefail
 set -o nounset
 echo "Bash options set"
 # Section 2: Health of dependent services
-postgres_ready() {
-    python << END
-import sys
-
-from psycopg2 import connect
-from psycopg2.errors import OperationalError
-import os
-
-try:
-    connect(
-        dbname=os.getenv('DJANGO_POSTGRES_DATABASE'),
-        user=os.getenv('DJANGO_POSTGRES_USER'),
-        password=os.getenv('DJANGO_POSTGRES_PASSWORD'),
-        host=os.getenv('DJANGO_POSTGRES_HOST'),
-        port=os.getenv"('DJANGO_POSTGRES_PORT'),
-    )
-except OperationalError:
-    sys.exit(-1)
-END
-}
-
-until postgres_ready; do
-  >&2 echo "Waiting for PostgreSQL to become available..."
-  sleep 5
-done
->&2 echo "PostgreSQL is available"
+#postgres_ready() {
+#    python << END
+#import sys
+#
+#from psycopg2 import connect
+#from psycopg2.errors import OperationalError
+#import os
+#
+#try:
+#    connect(
+#        dbname=os.getenv('DJANGO_POSTGRES_DATABASE'),
+#        user=os.getenv('DJANGO_POSTGRES_USER'),
+#        password=os.getenv('DJANGO_POSTGRES_PASSWORD'),
+#        host=os.getenv('DJANGO_POSTGRES_HOST'),
+#        port=os.getenv('DJANGO_POSTGRES_PORT'),
+#    )
+#except OperationalError:
+#    sys.exit(-1)
+#END
+#}
+#
+#until postgres_ready; do
+#  >&2 echo "Waiting for PostgreSQL to become available..."
+#  sleep 5
+#done
+#>&2 echo "PostgreSQL is available"
 
 # Section 3- Idempotent Django commands
 python manage.py collectstatic --noinput
