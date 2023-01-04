11acc393b5fa7b88a985678a274bec0a006670e9
crismigo <cristinamigo01@gmail.com>
2023-01-04 16:28:50 +0100

    #353 Seguir configurant servidors i automatitzacions CI/CD
    canvi nom de la base de dades

--- docker_ec2.py
+++ docker_ec2.py
@@ -25,7 +25,7 @@ command_create_dc_file = ["""echo "
                  
             environment:
               DJANGO_SECRET_KEY: 'om%_rbj(rdm*t$dt^!q)2o(3uztqzxtmv361d@j0lpza+q#zd)'
-              DJANGO_DATABASE_HOST: 'cloudformation-stack.cvktoxcvbtpd.eu-west-1.rds.amazonaws.com'
+              DJANGO_DATABASE_HOST: 'greenwheel-rds-pre.cvktoxcvbtpd.eu-west-1.rds.amazonaws.com'
               DJANGO_DATABASE_PORT: '5432'
               DJANGO_DATABASE_NAME: 'GreenWheelDB'
               DJANGO_DATABASE_USER: 'greenwheel'
